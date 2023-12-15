a042e3f22c057a76c4ba71ad0fa49abeea0d6721
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-12-15 11:20:10 +0400

    Updated code

--- Program.cs
+++ Program.cs
@@ -4,36 +4,50 @@ namespace ConsoleApp1
 {
   internal abstract class Program
   {
-    private static void s_storyPiramida(int[] data, int n)
+    private static void s_stortHeap(int[] arr)
     {
-      for (int i = n / 2; i >= 0; i--)
-      {
-        if (2 * i < n)
-          if (data[i] < data[2 * i])
-          {
-            var rotate = data[i];
-            data[i] = data[2 * i];
-            data[2 * i] = rotate;
-          }
+        int N = arr.Length;
+ 
+        for (int i = N / 2 - 1; i >= 0; i--)
+            s_heapifyMethod(arr, N, i);
 
-
-        if (2 * i + 1 < n)
-          if (data[i] < data[2 * i + 1])
-          {
-            var rotate = data[i];
-            data[i] = data[2 * i + 1];
-            data[2 * i + 1] = rotate;
-          }
-      }
-
+        for (int i = N - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+ 
+            s_heapifyMethod(arr, i, 0);
+        }
     }
 
-    /* Главный метод */    
+    private static void s_heapifyMethod(int[] arr, int N, int i)
+    {
+      int lessest = i; 
+      int l = 2 * i + 1; 
+      int r = 2 * i + 2; 
+
+      if (l < N && arr[l] < arr[lessest])
+        lessest = l;
+
+      if (r < N && arr[r] < arr[lessest])
+        lessest = r;
+
+      if (lessest != i)
+      {
+        int swap = arr[i];
+        arr[i] = arr[lessest];
+        arr[lessest] = swap;
+
+        s_heapifyMethod(arr, N, lessest);
+      }
+    }
+
+    /* Главный метод */
     public static void Main()
     {
       int[] arr = { 12, 11, 13, 5, 6, 7 };
       int length = arr.Length;
-      s_storyPiramida(arr, length);
+      s_stortHeap(arr);
 
       for (int i = 0; i < arr.Length; i++)
         Console.WriteLine(arr[i]);
