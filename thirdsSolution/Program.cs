24b1d1acc99e1c0fd7f833bd9e57531ac099804e
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-12-18 11:55:52 +0400

    Updated the second solution

---
+++ thirdsSolution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace thirdsSolution
+{
+  internal abstract class Program
+  {
+
+    /* Main method */
+    public static void Main()
+    {
+
+      Console.WriteLine();
+    }
+  }
+}
