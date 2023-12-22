bf3cd17b76965d658cf3f9274bd343d22cc097b7
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-12-22 20:00:40 +0400

    Updated the eighth solution

--- eighthSolution/Program.cs
+++ eighthSolution/Program.cs
@@ -1,79 +1,95 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace eighthSolution
+namespace eighthSolution;
+internal abstract class Program
 {
-    internal abstract class Program
-    {
-        #region Methods
-        /* Поиск максимального значения в блоке */
-        private static int s_findMaxValueOfBlockMethod(int[] arr, int left, int right)
-        {
-            // присваиваем первый элемент
-            int max = arr[0]; 
-
-            // проходимся по блоку
-            for (int i = left; i < right; i++)
-                // если элемент меньше элемента массива
-                if (max < arr[i])
-                    // то присваиваем его
-                    max = arr[i];
+  #region Variables
+  /* Создаём массив */
+  private static readonly int[] Array = { 1, 5, 2, 4, 6, 1, 3, 5, 7, 8, 9 };
+  /* Создаём размер блока */
+  private static readonly int N = (int)Math.Sqrt(Array.Length) + 1;
+  /* Массив максимальных элементов в блоке */
+  private static readonly int[] B = new int[N];
+  #endregion
   
-            // возвращаем максимальное значение
-            return max;
-        }
-
-        /* Поиск максимального значения в массиве */
-        private static int s_findMaxValueOfArrayMethod(int[] arr)
-        {
-            // создаём размерность N
-            int n = (int)Math.Sqrt(arr.Length) + 1;
-            // создаём размер блока
-            int blockSize = (arr.Length / n);
-            // создаём массив максимальных элементов
-            int[] arrayMax = new int[n];
-
-            // проходимся по блокам
-            for (int i = 0; i < n; i++)
-            {
-                // создаём начало блока
-                int arrLeft = i * blockSize;
-                // создаём конец блока
-                int arrRight = Math.Min((i + 1) * blockSize, arr.Length);
-                // ищем максимальный элемент и добалвяем его в массив
-                arrayMax[i] += s_findMaxValueOfBlockMethod(arr, arrLeft, arrRight);
-            }
-            
-            // вызываем поиск максимального элемента массива
-            // и возвращаем результат
-            return s_findMaxValueOfBlockMethod(arrayMax, 0, arrayMax.Length); 
-        }
-
-        /* Изменение элемента массива */
-        private static void s_getValueMethod(int[] arr, int[] maxArr, int i, int x)
-        {
-            arr[i] = x;
-            for (int j = 0; j < maxArr[j]; j++)
-            {
-
-            }
-        }
-        #endregion
-
-        /* Главный метод */
-        public static void Main()
-        {
-            // создаём массив
-            int[] array = { 1, 5, 2, 4, 6, 1, 3, 5, 7, 8, 1, 9 };
-
-            // выводим массив
-            Console.Write("Массив: ");
-            for (int i = 0; i < array.Length; i++)
-                Console.Write(array[i] + " ");
-            // выводим максимальный элемент
-            Console.WriteLine($"\nМаксимальный элемент в массиве: {s_findMaxValueOfArrayMethod(array)}");
-        }
+  #region Methods
+  /* Поиск максимального значения в диапазоне */
+  private static int s_findMaxValueMethod(int left, int right)
+  {
+    var max = Array[left];
+    var c1 = left / N;
+    var c2 = right / N;
+       
+    for (var i = 0; i < N; i++)
+    {
+      var arrLeft = i * N;
+      var arrRight = Math.Min((i + 1) * N, Array.Length);
+      B[i] += s_findMaxValueBlock(arrLeft, arrRight);
     }
+    
+    if (c1 == c2)
+    {
+      for (var i = left; i <= right; ++i)
+        if (max < Array[i])
+          max = Array[i];
+    }
+    else
+    {
+      for (int i = left, end = (c1 + 1) * N - 1; i <= end; ++i)
+        if (max < Array[i])
+          max = Array[i];
+      for (var i= c1 + 1; i <= c2 - 1; ++i)
+        if (max < Array[i])
+          max = Array[i];
+      for (var i= c2 * N; i <= right; ++i)
+        if (max < Array[i])
+          max = Array[i];
+    }
+    
+    return max; 
+  }
+
+  /* Нахождение максимального элемента в блоке */
+  private static int s_findMaxValueBlock(int left, int right)
+  {
+    var max = 0; 
+    
+    // проходимся по блоку
+    for (var i = left; i < right; i++)
+      // если элемент меньше элемента массива
+      if (max < Array[i])
+        // то присваиваем его
+        max = Array[i];
+  
+    // возвращаем максимальное значение
+    return max;
+  }
+
+  /* Изменение элемента массива */
+  private static void s_getValueMethod(int index, int value)
+  {
+    Array[index] = value;
+    var blockIndex = (index / N);
+    var max = Array[blockIndex * N]; 
+    for (var i = blockIndex * N; i < index + N; i++)
+      if (max < Array[i])
+        max = Array[i];
+
+    B[blockIndex] = max; 
+  }
+  #endregion
+
+  /* Главный метод */
+  public static void Main()
+  {
+    // выводим максимальный элемент
+    Console.WriteLine(s_findMaxValueMethod(1,3));
+    s_getValueMethod(8, 3);
+    
+    foreach (var value in Array)
+      Console.WriteLine(value);
+    foreach (var value in B)
+      Console.WriteLine(value);
+  }
 }
-
-
-// сайт с алгоритмами: https://e-maxx.ru/algo/sqrt_decomposition
