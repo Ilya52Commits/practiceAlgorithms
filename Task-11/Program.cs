3c980c830f388aa439521c1211e96970443a7dbc
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-12-23 21:35:01 +0400

    Finished the solutions

--- Task-11/Program.cs
+++ Task-11/Program.cs
@@ -1,120 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System; 
 
-namespace eleventhSolution;
+namespace Task_12;
 internal abstract class Program
 {
-  #region Method
-  /* Метод алгоритма Бойера и Мура */
-  private static void s_boyerMooreAlgorithmMethod(IReadOnlyList<char> text, IReadOnlyList<char> pat)
+  static void KMPSearch(string pat, string txt)
   {
-    // длина текста
-    var textCount = text.Count;
-    // длина элемента поиска
-    var patCount = pat.Count;
-    // сдвиг шаблона по отношению к тексту
-    var s = 0;
-    
-    var bpos = new int[patCount + 1];
-    var shift = new int[patCount + 1];
+    int M = pat.Length;
+    int N = txt.Length;
 
-    // инициализируйте все вхождения shift равным 0
-    for (var i = 0; i < patCount + 1; i++)
-      shift[i] = 0;
+    // Create lps[] that will hold the longest
+    // prefix suffix values for pattern
+    int[] lps = new int[M];
 
-    // выполните предварительную обработку
-    s_preprocessStrongSuffix(shift, bpos, pat, patCount);
-    s_preprocessSecondCase(shift, bpos, patCount);
+    // Index for pat[]
+    int j = 0;
 
-    while (s <= textCount - patCount)
-    {
-      var j = patCount - 1;
+    // Preprocess the pattern (calculate lps[]
+    // array)
+    computeLPSArray(pat, M, lps);
 
-      // Продолжайте уменьшать индекс j шаблона,
-      // пока символы шаблона и текста совпадают
-      // при этом сдвиге
-      while (j >= 0 && pat[j] == text[s + j])
-        j--;
-
-      // Если шаблон присутствует при текущем сдвиге,
-      // то индекс j станет равным -1 после
-      // описанного выше цикла
-      if (j < 0)
-      {
-        Console.Write($"Шаблонн содержится при сдвиге = {s}\n");
-        s += shift[0];
+    int i = 0;
+    while ((N - i) >= (M - j)) {
+      if (pat[j] == txt[i]) {
+        j++;
+        i++;
       }
-      else
-        // pat[i] != pat[s+j] сдвигайте шаблон
-        // shift[j+1] раз
-        s += shift[j + 1];
-    }
-  }
-
-  /* Предварительная обработка для случая 2 */
-  private static void s_preprocessSecondCase(IList<int> shift, IReadOnlyList<int> bpos, int m)
-  {
-    int i;
-    var j = bpos[0];
-    for (i = 0; i <= m; i++)
-    {
-      // установите положение границы первого
-      // символа шаблона для всех индексов в
-      // массиве shift, имеющих shift[i] = 0
-      if (shift[i] == 0)
-        shift[i] = j;
-
-      // если суффикс становится короче, чем
-      // bpos[0], используйте положение следующей
-      // по ширине границы в качестве значения j
-      if (i == j)
-        j = bpos[j];
-    }
-  }
-
-  /* Метод предварительной обработки суффикса */
-  private static void s_preprocessStrongSuffix(IList<int> shift, IList<int> bpos, IReadOnlyList<char> pat, int m)
-  {
-    // m - длина узора
-    int i = m, j = m + 1;
-    bpos[i] = j;
-
-    while (i > 0)
-    {
-      /* если символ в позиции i-1 не
-      эквивалентен символу в позиции j-1, то
-      продолжайте поиск границы справа от
-      шаблона */
-      while (j <= m && pat[i - 1] != pat[j - 1])
-      {
-        /* символ, предшествующий появлению
-        в шаблоне P, отличается от несоответствующего символа
-        в P, мы прекращаем пропускать вхождения
-        и сдвигаем шаблон с i на j */
-        if (shift[j] == 0)
-          shift[j] = j - i;
-
-        // обновите положение следующей границы
-        j = bpos[j];
+      if (j == M) {
+        Console.Write("Found pattern "
+                      + "at index " + (i - j));
+        j = lps[j - 1];
       }
-      /* p[i-1] совпадает с p[j-1], граница найдена.
-      сохраните начальное положение границы */
-      i--; j--;
-      bpos[i] = j;
+
+      // Mismatch after j matches
+      else if (i < N && pat[j] != txt[i]) {
+
+        // Do not match lps[0..lps[j-1]] characters,
+        // they will match anyway
+        if (j != 0)
+          j = lps[j - 1];
+        else
+          i = i + 1;
+      }
     }
   }
-  #endregion
 
-  /* Главный метод */
+  static void computeLPSArray(string pat, int M, int[] lps)
+  {
+    // Length of the previous longest prefix suffix
+    int len = 0;
+    int i = 1;
+    lps[0] = 0;
+
+    // The loop calculates lps[i] for i = 1 to M-1
+    while (i < M) {
+      if (pat[i] == pat[len]) {
+        len++;
+        lps[i] = len;
+        i++;
+      }
+      else // (pat[i] != pat[len])
+      {
+        // This is tricky. Consider the example.
+        // AAACAAAA and i = 7. The idea is similar
+        // to search step.
+        if (len != 0) {
+          len = lps[len - 1];
+
+          // Also, note that we do not increment
+          // i here
+        }
+        else // len = 0
+        {
+          lps[i] = len;
+          i++;
+        }
+      }
+    }
+  }
+
+  // Driver code
   public static void Main()
   {
-    // исходная строки
-    var text = "ABAAAABAACD".ToCharArray();
-    // элемент для поиска
-    var pat = "ABA".ToCharArray();
-    
-    // вывод результата алгоритма
-    Console.WriteLine("Поиск алгоритмом Бойера и Мура:");
-    s_boyerMooreAlgorithmMethod(text, pat);
+    string txt = "ABABDABACDABABCABAB";
+    string pat = "ABABCABAB";
+    KMPSearch(pat, txt);
   }
 }
